df1cb1d372d584e01b584c7b5998985f6e08c5d8
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-08-19 13:07:04 +0530

    svm completed

--- ml-algorithms-scratch/svm.py
+++ ml-algorithms-scratch/svm.py
@@ -10,30 +10,59 @@ class SVM():
         self.J_history = []
 
     def predict(self, X_i):
-        return (np.dot(self.w, X_i.T) - self.b)
+        approx = np.dot(self.w, X_i.T) - self.b
+        return np.sign(approx)
+
 
     def cost(self, X, y):
-        prediction = self.predict(X)
-        loss = y*prediction
-        return ((self.regularisation_lambda * ((self.w)**2)) + max(0, 1 - loss))
+        """"
+          Caculating the loss of the function.
+
+          Input :
+            X - np.array() - Input Feature Vectors
+            y - np.array() - Output Target Vector
+
+          Returns :
+            total_loss - int - Total lost of the function for the given data
+        """
+        prediction = np.dot(self.w, X.T) - self.b
+        prediction = y*prediction
+        hinge_loss = np.maximum(0, 1 - prediction)
+        loss = np.mean(hinge_loss)  
+        regularised_loss = np.sum(self.regularisation_lambda * ((abs(self.w))**2))
+        return (loss+regularised_loss)
 
     def fit(self, X, y):
-        self.w = np.zeros(X.shape[1])
         """"
-          Training the Linear SVM Algorithm with given data
+          Training the Linear SVM Algorithm with given data.
+
+          Input :
+            X - np.array() - Input Feature Vectors
+            y - np.array() - Output Target Vector
+
+          Returns :
+            J_History - list - Collection of Cost during each iterations
         """
+        self.w = np.zeros(X.shape[1])  #setting initial weights for the features as zeros
+        self.b = 0      #setting the bias value to zero
 
         for i in range(self.n_iters):
-            prediction  = self.predict(X)
-            self.J_history.append(self.cost(X,y))
-            if y*prediction >= 1:
-                dJW = 2 * self.regularisation_lambda * self.w
-                dJB = 0
-            else:
-                dJW = 2 * (self.regularisation_lambda * self.w) + np.dot(y, X.T)
-                dJB = y
+            loss = self.cost(X,y)
+            print(f"{i} Epochs : {loss:.2f} Loss")
+            self.J_history.append(loss)
+
+            for idx,x_i in enumerate(X):
+                condition = y[idx] * (np.dot(x_i, self.w) - self.b)
+                if condition >=1:
+                    dJW = 2 * self.regularisation_lambda * self.w
+                    dJB = 0
+                else:
+                    dJW = 2 * (self.regularisation_lambda * self.w) - np.dot(y[idx], x_i)
+                    dJB = y[idx]
 
             self.w = self.w - self.learning_rate * dJW
             self.b = self.b - self.learning_rate * dJB
+
+        return self.J_history
 
           
